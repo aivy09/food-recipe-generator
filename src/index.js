51218d935f7dcb110db0f0296395a3b4ce8c7053
aivy09 <147442706+aivy09@users.noreply.github.com>
2024-02-25 19:10:03 -0800

    Added API integration

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-function generateRecipe(event){
-    event.preventDefault();
-
+function displayRecipe(response){
     new Typewriter("#recipe", {
-        strings: "Flour",
+        strings: response.data.answer,
         autoStart: true,
         delay: 1,
         cursor: "",
-
 });
+}
 
+
+function generateRecipe(event){
+    event.preventDefault();
+
+    let inputInstructions = document.querySelector("#user-instructions");
+
+    let apiKey = "6513844e3b142o0a3et5b3de827d3a2f";
+    let prompt = `Select a highly rated food recipe about ${inputInstructions.value}`;
+    let context = "You are a food fanatic and have cooked many food recipes over the years. Display just the recipe in a list and in HTML format. Include title of recipe at the top and a link to the recipe page at the bottom.";
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    axios.get(apiURL).then(displayRecipe);
 }
 
 
